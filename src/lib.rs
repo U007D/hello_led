9914c1446f662027d89005c2092cc3e9f456f9a6
bRad <brad@humanenginuity.com>
2017-05-18 14:27:37 -0700

    "light light no light light light light.  Can you help?"  No need when you've got LIGHT!

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
-#![warn(missing_debug_implementations, missing_copy_implementations, trivial_casts, trivial_numeric_casts, unused_import_braces, unused_qualifications)]
 #![allow(non_camel_case_types)]
+#![warn(missing_debug_implementations, missing_copy_implementations, trivial_casts, trivial_numeric_casts, unused_import_braces, unused_qualifications)]
+#![deny(unused_must_use, overflowing_literals)]
 
 extern crate memmap;
-extern crate owning_ref;
+extern crate byteorder;
+extern crate num;
 
 type GeneralError = Box<std::error::Error>;
 type GeneralResult<T> = Result<T, GeneralError>;
@@ -12,27 +14,66 @@ type GeneralResult<T> = Result<T, GeneralError>;
 mod consts;
 use consts::*;
 use memmap::{Mmap, Protection};
-use owning_ref::OwningRef;
 use std::fs::OpenOptions;
+use std::ptr;
+use byteorder::{ByteOrder, LittleEndian, ReadBytesExt, WriteBytesExt};
+use std::io::Write;
 
 #[cfg(test)]
 mod unit_tests;
 
 pub fn lib_main(_args: Vec<String>) -> GeneralResult<()> {
-    let mmap = map_gpio()?;
-    let gpio = &*mmap;
+
+    //Set up GPIO
+    let mut gpio = Mapping::new(GPIO_BASE, GPIO_SIZE)?;
     println!("GPIO has been set up");
 
-    //Set GPIO4 (pin 7) as output
+    //Set GPIO5 (pin 29) as output
+    let gpclr = 0x28;
+    gpio.write(gpclr, 0b1 << 5)?;
+
+    let gpfsel0 = 0x00;
+    let gpio5_mask = 0b111 << 15;
+    let register = gpio.read(gpfsel0)? & !gpio5_mask;
+    gpio.write(gpfsel0, register | 0b1 << 15)?;
+    println!("GPIO5 set as output");
+
+    //flush?  hope not
+
+    //toggle
+    let gplev0 = 0x34;
+    println!("GPIO5: {}", gpio.read(gplev0)? & 0b1 << 5 >> 5);
+    let gpset = 0x1c;
+    gpio.write(gpset, 0b1 << 5)?;
+    println!("GPIO5 enabled");
+    println!("GPIO5: {}", gpio.read(gplev0)? & 0b1 << 5 >> 5);
+
     Ok(())
 }
 
-fn map_gpio() -> GeneralResult<OwningRef<Box<Mmap>, [u8]>> {
-    let fd = OpenOptions::new().read(true)
-                               .write(true)
-                               .open(consts::MEMORY_MAP_DEVICE_FILENAME)?;
-    let gpio_map = OwningRef::new(Box::new(Mmap::open_with_offset(&fd, Protection::ReadWrite,
-                                                                  GPIO_BASE,
-                                                                  GPIO_SIZE)?));
-    Ok(gpio_map.map(|owner| unsafe { owner.as_slice() }))
+struct Mapping {
+    map: Mmap,
+}
+
+impl Mapping {
+    fn new(base: usize, size: usize) -> GeneralResult<Mapping> {
+        //Map GPIO into user space
+        let fd = OpenOptions::new().read(true)
+                                   .write(true)
+                                   .open(MEMORY_MAP_DEVICE_FILENAME)?;
+        Ok(Mapping { map: Mmap::open_with_offset(&fd, Protection::ReadWrite,
+                                                   base,
+                                                   size)? })
+    }
+
+    fn read(&self, offset: usize) -> Result<u32, std::io::Error> {
+        Ok(unsafe { &(self.map.as_slice()[offset..offset + 4]) }
+        .read_u32::<LittleEndian>()?)
+    }
+
+    //extern crate memmap, extern crate byteorder are in effect
+    fn write(&mut self, offset: usize, value: u32) -> Result<(), std::io::Error> {
+        unsafe { &mut self.map.as_mut_slice()[offset..offset + 4] }
+            .write_u32::<LittleEndian>(value)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
-#![warn(missing_debug_implementations, missing_copy_implementations, trivial_casts, trivial_numeric_casts, unused_import_braces, unused_qualifications)]
 #![allow(non_camel_case_types)]
+#![warn(missing_debug_implementations, missing_copy_implementations, trivial_casts, trivial_numeric_casts, unused_import_braces, unused_qualifications)]
+#![deny(unused_must_use, overflowing_literals)]
 
 extern crate hello_led;
 mod consts;
